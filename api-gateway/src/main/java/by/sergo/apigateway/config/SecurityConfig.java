c1ae0f5e81294d00fcfe50cc15e8d192b1806273
SergeiKarachun <serryi_100@mail.ru>
2024-03-11 18:17:41 +0300

    add keycloak and security for gateway

---
+++ api-gateway/src/main/java/by/sergo/apigateway/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package by.sergo.apigateway.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+    @Autowired
+    private ReactiveClientRegistrationRepository registrationRepository;
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeExchange(exchange -> exchange
+                        .pathMatchers(HttpMethod.GET, "/actuator/**").permitAll()
+                        .anyExchange().authenticated()
+                )
+                .oauth2Login(Customizer.withDefaults())
+                .logout(logout -> logout.logoutSuccessHandler(oidcLogoutSuccessHandler()))
+        ;
+
+        return http.build();
+    }
+    @Bean
+    public ServerLogoutSuccessHandler oidcLogoutSuccessHandler() {
+        OidcClientInitiatedServerLogoutSuccessHandler successHandler = new OidcClientInitiatedServerLogoutSuccessHandler(registrationRepository);
+        successHandler.setPostLogoutRedirectUri("http://localhost:8080/login");
+        return successHandler;
+    }
+}
