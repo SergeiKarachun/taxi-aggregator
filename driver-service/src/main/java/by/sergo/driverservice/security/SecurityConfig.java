ccc9c1a2b5feb013c6ff35bcd7dbba048cb6d45c
SergeiKarachun <serryi_100@mail.ru>
2024-03-14 17:24:16 +0300

    add security config for all services

--- driver-service/src/main/java/by/sergo/driverservice/security/SecurityConfig.java
+++ driver-service/src/main/java/by/sergo/driverservice/security/SecurityConfig.java
@@ -20,7 +20,8 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(HttpMethod.GET, "/actuator/**").permitAll())
+                        .requestMatchers(HttpMethod.GET, "/actuator/**").permitAll()
+                        .anyRequest().authenticated())
                 .oauth2ResourceServer((oauth2) -> oauth2
                         .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthConverter)))
         ;
